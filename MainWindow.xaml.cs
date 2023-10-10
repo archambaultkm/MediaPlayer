d8b6194ec78dd5f17db4c33359a92df46544944b
prog2500 <prog2500@ML-RefVm-856249>
2023-10-10 15:44:46 -0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MediaPlayer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool userIsDragging;
+        private bool isPlaying;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Open_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void Open_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+
+        private void Play_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true; //TODO
+        }
+        private void Play_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            mePlayer.Play();
+            isPlaying = true;
+        }
+
+        private void Pause_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true; //TODO
+        }
+        private void Pause_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            mePlayer.Pause();
+            isPlaying = false;
+        }
+
+        private void Stop_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true; //TODO
+        }
+        private void Stop_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            mePlayer.Stop();
+            isPlaying = false;
+        }
+
+        private void Slider_DragStarted(object sender, DragStartedEventArgs e)
+        {
+            userIsDragging = true;
+        }
+
+        private void Slider_DragCompleted(object sender, DragCompletedEventArgs e)
+        {
+            userIsDragging = false;
+            mePlayer.Position = TimeSpan.FromSeconds(slider.Value);
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            lblProgress.Text = TimeSpan.FromSeconds(slider.Value).ToString(@"hh\:mm\:ss");
+        }
+    }
+}
