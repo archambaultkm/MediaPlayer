1b3474ea251cc6960f561d900e5df2d6bebd4a40
archambaultkm <archambaultkm@gmail.com>
2023-10-10 23:48:01 -0300

    ui built, can read in data from mp3 tags

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +14,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
+using System.Windows.Resources;
 using System.Windows.Shapes;
+using TagLib;
 
 namespace MediaPlayer
 {
@@ -21,6 +25,8 @@ namespace MediaPlayer
     /// </summary>
     public partial class MainWindow : Window
     {
+        TagLib.File currentFile;
+        private string currentFilePath;
         private bool userIsDragging;
         private bool isPlaying;
 
@@ -36,36 +42,66 @@ namespace MediaPlayer
 
         private void Open_Executed(object sender, ExecutedRoutedEventArgs e)
         {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "MP3 files (*.mp3;*.mpg;*.mpeg)|*.mp3;*.mpg;*.mpeg";
+
+
+            //TODO wrap with try/catch
+            if (fileDialog.ShowDialog() == true)
+            {
+                //set the media element source to the selected song
+                mediaPlayer.Source = new Uri(fileDialog.FileName);
+
+                //create a taglib file object for accessing mp3 metadata
+                currentFile = TagLib.File.Create(fileDialog.FileName);
+
+                //update the media information display
+                initMediaDisplay(currentFile);
+
+                //store the full path of the selected media for saving later
+                currentFilePath = fileDialog.FileName;
+            }
+        }
+
+        private void EditProperties_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void EditProperties_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
 
         }
 
         private void Play_CanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
-            e.CanExecute = true; //TODO
+            e.CanExecute = (mediaPlayer != null) && (mediaPlayer.Source != null);
         }
         private void Play_Executed(object sender, ExecutedRoutedEventArgs e)
         {
-            mePlayer.Play();
+            mediaPlayer.Play();
             isPlaying = true;
         }
 
         private void Pause_CanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
-            e.CanExecute = true; //TODO
+            //so long as media is already playing, pausing is valid
+            e.CanExecute = isPlaying; 
         }
         private void Pause_Executed(object sender, ExecutedRoutedEventArgs e)
         {
-            mePlayer.Pause();
+            mediaPlayer.Pause();
             isPlaying = false;
         }
 
         private void Stop_CanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
-            e.CanExecute = true; //TODO
+            //so long as media is already playing, stopping is valid
+            e.CanExecute = isPlaying; 
         }
         private void Stop_Executed(object sender, ExecutedRoutedEventArgs e)
         {
-            mePlayer.Stop();
+            mediaPlayer.Stop();
             isPlaying = false;
         }
 
@@ -77,12 +113,53 @@ namespace MediaPlayer
         private void Slider_DragCompleted(object sender, DragCompletedEventArgs e)
         {
             userIsDragging = false;
-            mePlayer.Position = TimeSpan.FromSeconds(slider.Value);
+            mediaPlayer.Position = TimeSpan.FromSeconds(slider.Value);
         }
 
         private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            lblProgress.Text = TimeSpan.FromSeconds(slider.Value).ToString(@"hh\:mm\:ss");
+            //update the progress label to reflect where the user has dragged the progress bar to
+            lblProgress.Text = TimeSpan.FromSeconds(slider.Value).ToString(@"hh\:mm\:ss"); 
+        }
+
+        private void initMediaDisplay(TagLib.File file)
+        {
+            //update the displayed media information
+
+            mediaName.Text = file.Tag.Title;
+            mediaArtist.Text = file.Tag.FirstAlbumArtist;
+            mediaAlbum.Text = file.Tag.Album;
+
+            //set image art if the mp3 file has album art attached
+            if (file.Tag.Pictures.Length > 0)
+            {
+                var image = file.Tag.Pictures[0];
+                albumArt.Source = getAlbumArtAsBitmap(image);
+            }
+
+            
+        }
+
+        private BitmapImage getAlbumArtAsBitmap (IPicture image)
+        {
+            //code for converting to ImageSource found here, a comination of both answers:
+            //https://stackoverflow.com/questions/35786467/wpf-c-sharp-image-source 
+
+            var imageBytes = image.Data.Data;
+
+            Uri BitmapUri;
+            StreamResourceInfo BitmapStreamSourceInfo;
+            try
+            {
+                BitmapImage bi = new BitmapImage();
+
+                bi.BeginInit();
+                bi.StreamSource = new MemoryStream(imageBytes);
+                bi.EndInit();
+
+                return bi;
+            }
+            catch { throw; }
         }
     }
 }
